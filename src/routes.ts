1b8d3e7f90037f58d2114b656f464cf236c29824
bruna-gregorio <blgregorio16@gmail.com>
2021-11-10 16:32:30 -0300

    altenticação do usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { AuthenticateUserController } from "./controllers/AuthenticateUserController";
 
 import { CreateUserController } from "./controllers/CreateUserController";
 import { DeleteUserController } from "./controllers/DeleteUserController";
@@ -8,11 +9,15 @@ import { UpdatePasswordController } from "./controllers/UpdatePasswordController
 
 const routes = Router()
 
+const authenticateUserController = new AuthenticateUserController()
+
 const createUserController = new CreateUserController()
 const listUsersController = new ListUsersController()
 const updateNameController = new UpdateNameController()
 const updatePasswordController = new UpdatePasswordController()
 const deleteUsersController = new DeleteUserController()
+
+routes.post("/login", authenticateUserController.handle)
 
 routes.post("/users", createUserController.handle)
 routes.get("/users", listUsersController.handle)
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getCustomRepository } from "typeorm"
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+
+import { UserRepositories } from "../repositories/UserRepositories"
+
+
+interface IAuthenticateUser {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+  async execute({ email, password }: IAuthenticateUser) {
+    const userRepositories = getCustomRepository(UserRepositories)
+
+    const user = userRepositories.findOne({
+      email
+    })
+
+    if (!user) {
+      throw new Error("Email/Password incorrect!")
+    }
+
+    const passwordMatch = await compare(password, (await user).password)
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect!")
+    }
+
+    const token = sign(
+      {
+        email: (await user).email,
+      },
+      "67b440626dd418fd9956ccbb336ee459",
+      {
+        subject: (await user).id, expiresIn: "1d"
+      }
+    )
+
+    return token
+  }
+}
+
+export { AuthenticateUserService }
