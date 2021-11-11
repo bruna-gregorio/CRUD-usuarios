08ec3c61a89f9e089f7106f7b24261fff3f635be
bruna-gregorio <blgregorio16@gmail.com>
2021-11-11 15:48:01 -0300

    autenticando o usuario para alterar informações

--- src/routes.ts
+++ src/routes.ts
@@ -22,9 +22,9 @@ routes.post("/login", authenticateUserController.handle)
 
 routes.post("/users", createUserController.handle)
 routes.get("/users", listUsersController.handle)
-routes.put("/users/name/:id", updateUserController.updateName)
-routes.put("/users/email/:id", updateUserController.updateEmail)
-routes.put("/users/password/:id", updateUserController.updatePassword)
+routes.put("/users/name/:id", ensureAuthenticate, updateUserController.updateName)
+routes.put("/users/email/:id", ensureAuthenticate, updateUserController.updateEmail)
+routes.put("/users/password/:id", ensureAuthenticate, updateUserController.updatePassword)
 routes.delete("/users/:id", ensureAuthenticate, deleteUsersController.handle)
 
 export { routes }
